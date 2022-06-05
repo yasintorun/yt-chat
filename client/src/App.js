b2b46ac825cefd215796d149ac68018b15bf698f
Yasin Torun <yasintorun4680@gmail.com>
2022-06-05 10:35:26 +0300

    Added socket.io-client package

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
+import React, { useEffect, useState } from 'react'
+import io from 'socket.io-client'
+
+const socket = io("http://localhost:4001")
+
+socket.on("message", (msg) => {
+  console.log(msg)
+})
 
 function App() {
 
   return (
     <div className="App">
+      {socket ? (
+        <p>Connected</p>
+      ) : (
+        <p>Not Connected</p>
+      )}
     </div>
   );
 }
